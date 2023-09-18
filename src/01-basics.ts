62ddcbf14024ad0f768c065f2e829d58040d20b4
Stephen Power <stephenxpower2003@gmail.com>
2023-09-18 11:29:12 +0100

    Added type-safety to colleague data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -20,6 +20,16 @@ const friend1 = {
   console.log(friends[1]);
   
   //   -------------------
+
+  interface Colleague {
+    name: string;
+    department: string;
+    contact: {
+        email: string;
+        extention: number
+    }
+}
+
   const colleague1 = {
     name: "Ralph Graham",
     department: "Engineering",
