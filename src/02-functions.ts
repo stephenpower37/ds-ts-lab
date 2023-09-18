349f3c076cdcb20c57db021ea6f92b4c24ff43ef
Stephen Power <stephenxpower2003@gmail.com>
2023-09-18 18:21:10 +0100

    Typed function signature.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,1 +1,45 @@
 import {Friend, Colleague } from './myTypes'
+import { friends, colleagues } from "./01-basics";
+
+function older(f: Friend) : string {
+     f.age += 1
+     return `${f.name} is now ${f.age}` 
+}
+
+function allOlder(f: Friend[]) : string[]{
+    let Older: string[] = [];
+    f.forEach(
+        (f) => {
+            f.age += 1;
+            Older.push(`${f.name} is now ${f.age}` );
+        }
+    )
+    return Older;
+}
+
+console.log(older(friends[0]))
+console.log(allOlder(friends))
+
+// Find the colleague with the highest extension number.
+function highestExtension(cs: Colleague[]): Colleague {
+    const result = cs.sort(
+      (c1, c2) => c1.contact.extension - c2.contact.extension
+    );
+    return result[cs.length - 1];
+  }
+  console.log(highestExtension(colleagues.current));
+
+  function addColleague(cs: Colleague[], name: string, department: string, email: string){
+    const newColleague: Colleague = {
+        name: name,
+        department: department,
+        contact: {
+            email: email,
+            extension: highestExtension(cs).contact.extension + 1
+        }
+    }
+    cs.push(newColleague);
+}
+
+addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
+console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
