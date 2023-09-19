f4c9f8a69ed4682c880634c98bb03687b49a0a83
Stephen Power <stephenxpower2003@gmail.com>
2023-09-19 10:25:16 +0100

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -71,3 +71,15 @@ console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name
 
   console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
   console.log(findFriends(friends, (friend) => friend.age < 35));
+
+  function addInterest(friend:Friend, interest: string) {
+    if (friend.interests != undefined) {
+        friend.interests.push(interest)
+    } 
+    else {
+      friend.interests = [interest]
+    }
+    return friend.interests
+}
+
+console.log(addInterest(friends[1], 'Politics'))
